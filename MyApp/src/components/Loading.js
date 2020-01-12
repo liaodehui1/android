61df3c6d88eb7e20b890225d147cf021e78817e8
liaodehui <1959956996@qq.com>
2020-01-12 23:31:42 +0800

    fix-loading

--- MyApp/src/components/Loading.js
+++ MyApp/src/components/Loading.js
@@ -1,6 +1,14 @@
 import React, { PureComponent } from 'react';
-import {  View, Text, ActivityIndicator } from 'react-native';
+import {  View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 
+const styles = StyleSheet.create({
+  Loading: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: "center",
+    height: 30
+  }
+})
 export default class Loading extends PureComponent {
   constructor(props) {
     super(props);
@@ -8,7 +16,7 @@ export default class Loading extends PureComponent {
 
   render() {
     return (
-      <View style={{flexDirection: "row",alignItems: 'center',justifyContent: "center"}}>
+      <View style={styles.Loading}>
         <ActivityIndicator size={this.props.size || 'small'} 
           color={this.props.color || '#00ff00'}></ActivityIndicator>
         <Text>{this.props.text || '正在加载...'}</Text>
