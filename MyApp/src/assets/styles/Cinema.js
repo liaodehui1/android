ebf22336c7b77dd05e66f89c1a25d831f96c32ee
liaodehui <1959956996@qq.com>
2020-01-19 22:51:26 +0800

    影院

--- MyApp/src/assets/styles/Cinema.js
+++ MyApp/src/assets/styles/Cinema.js
@@ -1,9 +1,40 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
+  topbar: {
+    height: 44,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#e6e6e6',
+    borderStyle: 'solid',
+    backgroundColor: '#f5f5f5',
+    flexDirection: 'row',
+  }, 
+  searchInput: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    height: 28,
+    backgroundColor: '#fff',
+    marginHorizontal: 15,
+    borderRadius: 5,
+    borderColor: '#e6e6e6',
+    borderStyle: 'solid',
+    borderWidth: 0.5
+  },
+  searchImg: {
+    width: 14,
+    height: 14,
+    marginHorizontal: 4,
+    tintColor: '#b2b2b2'
+  },
+  searchTip: {
+    fontSize: 13,
+    color: '#b2b2b2'
+  },
   wrapper: {
     paddingLeft: 10,
-    paddingRight: 0,
-    paddingBottom: 38
+    flex: 1
   }
 })
