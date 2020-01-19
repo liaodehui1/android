64b5bd4e1d2485c60aa59ed0f4c4e184d8e3d9ee
liaodehui <1959956996@qq.com>
2020-01-19 22:51:41 +0800

    电影

--- MyApp/src/assets/styles/Movie.js
+++ MyApp/src/assets/styles/Movie.js
@@ -8,32 +8,6 @@ export default StyleSheet.create({
     borderStyle: 'solid',
     borderBottomWidth: 0.5
   },
-  cityEntry: {
-    flexBasis: 70,
-  },
-  cityBox: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  cityName: {
-    width: 50,
-    lineHeight: 40,
-    fontSize: 15,
-    color: '#666',
-    textAlign: 'right',
-    paddingRight: 3
-  },
-  cityEntryArrow: {
-    width: 0,
-    height: 0,
-    borderWidth: 4,
-    borderStyle: 'solid',
-    borderTopColor: '#b0b0b0',
-    borderBottomColor: 'rgba(0,0,0,0)',
-    borderLeftColor: 'rgba(0,0,0,0)',
-    borderRightColor: 'rgba(0,0,0,0)',
-    marginTop: 5
-  },
   tabs: {
     flex: 1,
     flexDirection: 'row',
